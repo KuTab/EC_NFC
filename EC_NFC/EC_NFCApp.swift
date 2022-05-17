53e3e07e513a7572f6d8315ec362306d829ecd15
Egor Dadugin <egor140202@yandex.ru>
2022-05-17 19:45:09 +0300

    Initial Commit

---
+++ EC_NFC/EC_NFCApp.swift
@@ -0,0 +1,17 @@
+//
+//  EC_NFCApp.swift
+//  EC_NFC
+//
+//  Created by Egor Dadugin on 17.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct EC_NFCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
